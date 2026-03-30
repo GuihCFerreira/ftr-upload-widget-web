1e4f0a1d677bae60e076f2479f5e086edaf544a1
Guilherme de Carvalho Ferreira <guihhh231@gmail.com>
2026-03-30 19:16:12 -0300

    feat: add basic widget animations with motion

--- src/components/upload-widget-upload-item.tsx
+++ src/components/upload-widget-upload-item.tsx
@@ -1,10 +1,16 @@
 import { Download, ImageUp, Link2, RefreshCcw, X } from "lucide-react";
 import { Button } from "./ui/button";
 import * as Progress from "@radix-ui/react-progress";
+import { motion } from 'motion/react'
 
 export function UploadWidgetUploadItem() {
   return (
-    <div className="p-3 rounded-lg flex flex-col gap-3 shadow-shape-content bg-white/2 relative overflow-hidden">
+    <motion.div
+      className="p-3 rounded-lg flex flex-col gap-3 shadow-shape-content bg-white/2 relative overflow-hidden"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.4 }}
+    >
 
       <div className="flex flex-col gap-1">
 
@@ -56,6 +62,6 @@ export function UploadWidgetUploadItem() {
 
       </div>
 
-    </div>
+    </motion.div>
   )
 }
